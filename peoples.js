47d4f7099d8ed802a8a97026592d0511570aeff2
manjotplaha <manjot123@gmail.com>
2023-10-09 15:54:43 -0400

    importing and epxporting

---
+++ peoples.js
@@ -0,0 +1,5 @@
+const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const age = [20,30,40,50];
+
+console.log(people);
+module.exports = {people, age};
--- test.js
+++ test.js
@@ -1,3 +1,20 @@
-const name = 'mario';
+// const name = 'mario';
 
-console.log(name);
+// console.log(name);
+
+// const greet = (name) => {
+//   console.log(`hello, ${name}`);
+// }
+
+// greet('mario');
+// greet('yoshi');
+
+// console.log(global);
+
+console.log(__dirname);
+console.log(__filename);
+
+setTimeout(() => {
+  console.log('in the timeout');
+  clearInterval(int);
+}, 3000);
